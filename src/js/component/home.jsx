9134d96716dcc81950e51965d713522f23cf2ce3
Jose Hernandez <JOSERAFAHERNANDEZ.98@GMAIL.COM>
2024-08-11 08:19:11 +0000

    fixing things

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,37 +2,12 @@ import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import TraffickLight from "./trafficlight";
 
 //create your first component
 const Home = () => {
-	const [changeColor,setChangeColor]=useState("grey")
-	function changingColor(color){
-		setChangeColor(color);
-	}
-	return (
-		<>
-		<div className="col-12 mx-auto" id="traffickLight">
-		<div className="col-12" id="redLight">
-		  <button 
-			onClick={() => changingColor("red")} 
-			style={{ backgroundColor: changeColor === "red" ? "red" : "white" }}>
-		  </button>
-		</div>
-		<div className="col-12" id="yellowLight">
-		  <button 
-			onClick={() => changingColor("yellow")} 
-			style={{ backgroundColor: changeColor === "yellow" ? "yellow" : "white" }}>
-		  </button>
-		</div>
-		<div className="col-12" id="greenLight">
-		  <button 
-			onClick={() => changingColor("green")} 
-			style={{ backgroundColor: changeColor === "green" ? "green" : "white" }}>
-		  </button>
-		</div>
-		</div>
-	  </>
-	)
+return (
+	<TraffickLight></TraffickLight>
+)
 }
-
 export default Home;
--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -1,2 +1,34 @@
-//pseudocodigo
-//al presionar el boton funcione
+
+import React, { useState } from "react";
+
+const TraffickLight = () => {
+	const [changeColor,setChangeColor]=useState("grey")
+	function changingColor(color){
+		setChangeColor(color);
+	}
+	return (
+		<>
+		<div className="col-12 mx-auto" id="traffickLight">
+		<div className="col-12" id="redLight">
+		  <button 
+			onClick={() => changingColor("red")} 
+			style={{ backgroundColor: changeColor === "red" ? "red" : "white" }}>
+		  </button>
+		</div>
+		<div className="col-12" id="yellowLight">
+		  <button 
+			onClick={() => changingColor("yellow")} 
+			style={{ backgroundColor: changeColor === "yellow" ? "yellow" : "white" }}>
+		  </button>
+		</div>
+		<div className="col-12" id="greenLight">
+		  <button 
+			onClick={() => changingColor("green")} 
+			style={{ backgroundColor: changeColor === "green" ? "green" : "white" }}>
+		  </button>
+		</div>
+		</div>
+	  </>
+	)
+}
+export default TraffickLight
